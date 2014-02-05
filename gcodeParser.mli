dc37a313aaa54111e61a84a3b717ac98609d9761
Erkki Seppälä <flux@modeemi.fi>
2014-02-05 09:09:16 +0200

    GcodeParser: added interface. Fixed parse_gcode input.

---
+++ gcodeParser.mli
@@ -0,0 +1,28 @@
+type position = {
+  x : float option;
+  y : float option;
+  z : float option;
+  e : float option;
+  rest : string;
+}
+
+type rest = string
+
+type move = G0 | G1
+
+type input =
+    Move of (move * position)
+  | G90abs of rest
+  | G91rel of rest
+  | G92 of position
+  | Other of string
+
+val string_of_gfloat : float -> string
+
+val string_of_token : GcodeLexer.token -> string
+
+val parse_gcode : Lexing.lexbuf -> input BatEnum.t
+
+val string_of_input :
+  ?mode:[< `Absolute | `Relative > `Absolute ] ->
+  ?previous:input -> input -> string
